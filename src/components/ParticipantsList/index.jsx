d752b9f920eee0b7c2ad80f0e84207019c7ccc5e
Valentin Segura <valentin.segura.h@gmail.com>
2022-11-28 19:58:15 -0300

    resaltar usuario logueado en listado de participantes

--- src/components/ParticipantsList/index.jsx
+++ src/components/ParticipantsList/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,24 +10,36 @@ import Paper from '@mui/material/Paper';
 import './styles.scss';
 
 const ParticipantsList = ({ participants }) => {
+    const { user } = useSelector((state) => state.session);
 
     return (
         <div className="participants-list">
             <TableContainer className="participants-table" component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableBody>
-                        {participants.map((participant) => (
-                            <TableRow
-                                key={participant.id}
-                                className="participants-table-row"
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {participant.userName}
-                                </TableCell>
-                                <TableCell align="right">{participant.totalScore}</TableCell>
-                            </TableRow>
-                        ))}
+                        {participants.map((participant) => {
+                            let styles = null;
+                            if (user.name === participant.userName) {
+                                styles = {
+                                    color: '#54086F',
+                                    fontWeight: 'bold',
+                                };
+                            }
+                            return (
+                                <TableRow
+                                    key={participant.id}
+                                    className="participants-table-row"
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row" style={styles}>
+                                        {participant.userName}
+                                    </TableCell>
+                                    <TableCell align="right" style={styles}>
+                                        {participant.totalScore}
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
                     </TableBody>
                 </Table>
             </TableContainer>
