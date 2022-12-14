7583b714fb750987b144599855f327f7105c0567
Valentin Segura <valentin.segura.h@gmail.com>
2022-12-14 09:49:59 -0300

    carpeta User en components y pages

---
+++ src/pages/User/PencaEventDetail/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import EventDetail from '../../../components/User/EventDetail';
+import Navbar from '../../../components/Navbar';
+import PencaDetailHeader from '../../../components/User/PencaDetailHeader';
+import Spinner from '../../../components/Spinner';
+import useEvent from '../../../hooks/User/useEvent';
+import usePenca from '../../../hooks/User/usePenca';
+import { USER_LOGGED_PAGES, USER_ROUTES } from '../../../utils/navbarItems';
+
+const PencaEventDetail = () => {
+    let params = useParams();
+    const navigate = useNavigate();
+    const {loading: loadingPenca, penca} = usePenca(params.pencaId);
+    const {loading: loadingEvent, event, stats} = useEvent(params.pencaId, params.eventId);
+
+    if (loadingPenca || loadingEvent) return <Spinner />;
+
+    const redirectAfterSave = () => {
+        navigate(`/pencas/${params.pencaId}/eventos`);
+    };
+
+    return (
+        <>
+            <Navbar
+                pages={USER_LOGGED_PAGES}
+                routes={USER_ROUTES}
+            />
+            {penca ? (
+                <PencaDetailHeader penca={penca} />
+            ) : null}
+            {event ? (
+                <EventDetail
+                    event={event}
+                    stats={stats}
+                    redirectAfterSave={redirectAfterSave}
+                />
+            ) : null}
+        </>
+    );
+};
+
+export default PencaEventDetail;
---
+++ src/pages/User/Register/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Navbar from '../../../components/Navbar';
+import SignupForm from '../../../components/User/SignupForm';
+import { USER_PAGES, USER_ROUTES } from '../../../utils/navbarItems';
+
+const Register = () => {
+
+    return (
+        <>
+            <Navbar
+                pages={USER_PAGES}
+                routes={USER_ROUTES}
+            />
+            <SignupForm />
+        </>
+    );
+};
+
+export default Register;
